e7eb5f5e90875434e2d5d4b01e3dc8cb8f5c4c50
Killian <knolan9119@gmail.com>
2022-12-15 17:57:02 +0000

    Combined data_transformation and data_preprocessor.

--- src/data_preprocessor.py
+++ src/data_preprocessor.py
@@ -8,13 +8,13 @@ pd.options.mode.chained_assignment = None
 class WirelessDataPreProcessor:
     def __init__(self, dataframe, is_complete=True):
         self.__df = dataframe
-        self.no_of_features = len(self.__df.columns)
-        self.no_of_examples = self.__df.size
-        self.numeric_columns = ["SNR", "CQI", "RSSI", "NRxRSRP", "NRxRSRQ", "ServingCell_Distance"]
-        self.categorical_columns = list(set(self.__df.columns).difference(self.numeric_columns))
-        self.group_columns = ["movement_type", "session"]
-        self.geo_columns = ["ServingCell_Lon", "ServingCell_Lat"]
-        self.interest_columns = ["DL_bitrate", "UL_bitrate"]
+        self.__no_of_features = len(self.__df.columns)
+        self.__no_of_examples = self.__df.size
+        self.__numeric_columns = ["SNR", "CQI", "RSSI", "NRxRSRP", "NRxRSRQ", "ServingCell_Distance"]
+        self.__categorical_columns = list(set(self.__df.columns).difference(self.__numeric_columns))
+        self.__group_columns = ["movement_type", "session"]
+        self.__geo_columns = ["ServingCell_Lon", "ServingCell_Lat"]
+        self.__interest_columns = ["DL_bitrate", "UL_bitrate"]
 
         self.__df = self.__df.replace({"-": np.nan})
         self.__df["Timestamp"] = pd.to_datetime(self.__df["Timestamp"], format="%Y.%m.%d_%H.%M.%S")
@@ -47,20 +47,19 @@ class WirelessDataPreProcessor:
         self.__df = new_df
 
     def __impute_missing_data(self, dataframe):
-        dataframe[self.geo_columns].ffill(inplace=True)
+        dataframe[self.__geo_columns].ffill(inplace=True)
         scaler = StandardScaler()
-        x_data = dataframe[self.numeric_columns]
+        x_data = dataframe[self.__numeric_columns]
         normalized_x_data = scaler.fit_transform(x_data)
         imputer = KNNImputer(n_neighbors=5)
         imputed_normalized_array = imputer.fit_transform(normalized_x_data)
         imputed_array = scaler.inverse_transform(imputed_normalized_array)
-        dataframe[self.numeric_columns] = imputed_array
+        dataframe[self.__numeric_columns] = imputed_array
         return dataframe
 
     def __create_history_column(self, dataframe, no_of_seconds=10):
-        for column in self.interest_columns:
+        for column in self.__interest_columns:
             new_column = "Average_" + column
-            no_of_columns = len(dataframe.columns)
             history_column = []
             base_column = dataframe[column]
             # This loop creates history values for the first k rows,
@@ -77,3 +76,13 @@ class WirelessDataPreProcessor:
 
     def get_df(self):
         return self.__df
+
+
+if __name__ == "__main__":
+    raw_data = pd.read_csv("C:/Users/knola/Desktop/Final Year Project/Datasets/Raw/all_4G_data.csv", index_col=None)
+
+    raw_data = raw_data #[raw_data["movement_type"] == "static"]
+
+    imputed_data = WirelessDataPreProcessor(raw_data).get_df()
+
+    imputed_data.to_csv("transformed_4G_data.csv", index=False, encoding="utf-8")
