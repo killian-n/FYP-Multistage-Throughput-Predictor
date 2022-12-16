479149dfa90649f3aeb76324d90d06fe78036c05
Killian <knolan9119@gmail.com>
2022-12-16 00:42:07 +0000

    Completed WirelessDataPreprocessor functions.
    - Categorical features will now be encoded using one hot encoding,
      see WirelessDataPreprocessor.one_hot_encode().
    
    - X and Y sequences can be created from the transformed data using
      WirelessDataPreprocessor.create_sequences() or
      WirelessDataPreprocessor.create_grouped_sequences()
    
    - All numeric columns will be normalised and scaled using
      WirelessDataPreprocessor.scale_data(). Scaler will be saved as a class
      variable.
    
    - Scaler has getter WirelessDataPreprocessor.get_scaler()
    
    - Fixed some numeric columns being absent.

--- src/data_preprocessor.py
+++ src/data_preprocessor.py
@@ -6,58 +6,89 @@ pd.options.mode.chained_assignment = None
 
 
 class WirelessDataPreProcessor:
-    def __init__(self, dataframe, is_complete=True):
+    def __init__(self, dataframe, is_grouped=True, manual_mode=False):
+
+        # Initial class variables
         self.__df = dataframe
-        self.__no_of_features = len(self.__df.columns)
-        self.__no_of_examples = self.__df.size
-        self.__numeric_columns = ["SNR", "CQI", "RSSI", "NRxRSRP", "NRxRSRQ", "ServingCell_Distance"]
-        self.__categorical_columns = list(set(self.__df.columns).difference(self.__numeric_columns))
-        self.__group_columns = ["movement_type", "session"]
-        self.__geo_columns = ["ServingCell_Lon", "ServingCell_Lat"]
+        self.__numeric_columns = ["SNR", "CQI", "RSSI", "NRxRSRP", \
+            "NRxRSRQ", "ServingCell_Distance", "Speed", "RSRQ", "RSRP"]
+        self.__geo_columns = ["Longitude", "Latitude", "ServingCell_Lon", "ServingCell_Lat"]
         self.__interest_columns = ["DL_bitrate", "UL_bitrate"]
+        self.__historic_columns = ["Average_DL_bitrate", "Average_UL_bitrate"]
 
+        self.__categorical_columns = list(set(self.__df.drop(columns=["Timestamp"])).difference(\
+            self.__df[self.__numeric_columns+self.__geo_columns+self.__interest_columns]))
+
+        # Basic formatting
         self.__df = self.__df.replace({"-": np.nan})
-        self.__df["Timestamp"] = pd.to_datetime(self.__df["Timestamp"], format="%Y.%m.%d_%H.%M.%S")
-        self.__df[["RSRQ", "SNR", "CQI", "RSSI", "NRxRSRP", "NRxRSRQ", "ServingCell_Distance",
-                  "ServingCell_Lat", "ServingCell_Lon", "DL_bitrate", "UL_bitrate"]] \
-            = self.__df[["RSRQ", "SNR", "CQI", "RSSI", "NRxRSRP", "NRxRSRQ", "ServingCell_Distance",
-                        "ServingCell_Lat", "ServingCell_Lon", "DL_bitrate", "UL_bitrate"]].astype(float)
+        self.__df = self.__df.replace({"": np.nan})
+        try:
+            self.__df["Timestamp"] = pd.to_datetime(self.__df["Timestamp"],
+             format="%Y.%m.%d_%H.%M.%S")
+        except Exception as e:
+            pass
+        self.__df[self.__numeric_columns+self.__interest_columns] \
+            = self.__df[self.__numeric_columns+self.__interest_columns].astype(float)
 
-        if is_complete:
-            self.__impute_grouped_missing_data()
-            self.__create_grouped_history_column()
-        else:
-            self.__df = self.__impute_missing_data(self.__df)
-            self.__df = self.__create_history_column(self.__df)
 
-    def __impute_grouped_missing_data(self):
+        # Output variables for use in models
+        self.__scaler = None
+        self.__x_sequences = []
+        self.__y_sequences = []
+
+        # Options relating to the input shape of the dataframe
+        if not manual_mode:
+            if is_grouped:
+                self.__categorical_columns.remove("session")
+                self.impute_grouped_missing_data()
+                self.create_grouped_history_column()
+            else:
+                self.__df = self.impute_missing_data(self.__df)
+                self.__df = self.create_history_column(self.__df)
+            self.one_hot_encode()
+            self.scale_data()
+
+    def impute_grouped_missing_data(self):
+        '''
+        Groups data by "movement_type" and uses function self.impute_missing_data() to 
+        fill in missing data using KNN for each movement_type seperately.
+        This is better than imputing over the entire column as the movement type plays a large
+        role in the distribution of the data-points.
+
+        Assigns updated dataframe to df.
+        '''
         groups = self.__df.groupby("movement_type")
         new_df = pd.DataFrame()
         for name, group in groups:
-            imputed_df = self.__impute_missing_data(group)
+            imputed_df = self.impute_missing_data(group)
             new_df = pd.concat([new_df, imputed_df], axis=0)
         self.__df = new_df
 
-    def __create_grouped_history_column(self):
+    def create_grouped_history_column(self):
         groups = self.__df.groupby("session")
         new_df = pd.DataFrame()
         for name, group in groups:
-            altered_data = self.__create_history_column(group)
+            altered_data = self.create_history_column(group)
             new_df = pd.concat([new_df, altered_data], axis=0)
         self.__df = new_df
 
-    def __impute_missing_data(self, dataframe):
-        dataframe[self.__geo_columns].ffill(inplace=True)
+    def impute_missing_data(self, dataframe):
+        # Long, lat positions of the towers get imputed using foward fill
+        dataframe[self.__geo_columns] = dataframe[self.__geo_columns].fillna(method="ffill")
+        # Numeric features use the KNNImputer
         scaler = StandardScaler()
-        x_data = dataframe[self.__numeric_columns]
+        x_data = dataframe[self.__numeric_columns+self.__interest_columns]
         normalized_x_data = scaler.fit_transform(x_data)
         imputer = KNNImputer(n_neighbors=5)
         imputed_normalized_array = imputer.fit_transform(normalized_x_data)
         imputed_array = scaler.inverse_transform(imputed_normalized_array)
-        dataframe[self.__numeric_columns] = imputed_array
+        dataframe[self.__numeric_columns+self.__interest_columns] = imputed_array
+        # Categorical features use most frequent
+        dataframe[self.__categorical_columns] = dataframe[self.__categorical_columns]\
+            .fillna(dataframe[self.__categorical_columns].mode().iloc[0])
         return dataframe
 
-    def __create_history_column(self, dataframe, no_of_seconds=10):
+    def create_history_column(self, dataframe, no_of_seconds=10):
         for column in self.__interest_columns:
             new_column = "Average_" + column
             history_column = []
@@ -74,15 +105,79 @@ class WirelessDataPreProcessor:
             dataframe[new_column] = history_column
         return dataframe
 
+    def one_hot_encode(self):
+        categorical_features = self.__categorical_columns+["Timestamp", "session"]
+        cols = [i for i in self.__df.columns if i not in categorical_features]
+        self.__df[cols] = self.__df[cols].astype(float)
+        self.__df = pd.get_dummies(self.__df)
+        self.__categorical_columns = list(set(self.__df.drop(columns=["Timestamp"])).difference(\
+            self.__df[self.__numeric_columns+self.__geo_columns+self.__interest_columns]))
+
+    def scale_data(self):
+        scaler = StandardScaler()
+        x_data = self.__df[self.__numeric_columns+self.__geo_columns+\
+            self.__historic_columns+self.__interest_columns]
+        normalized_x_data = scaler.fit_transform(x_data)
+        self.__df[self.__numeric_columns+self.__geo_columns+self.__historic_columns+\
+            self.__interest_columns] = normalized_x_data
+        self.__scaler = scaler
+
+    def create_grouped_sequences(self, history_length=10, horizon_length=5, 
+     include_columns=[], exclude_columns=["Timestamp", "CellID", "DL_bitrate", "UL_bitrate"],
+     y_columns=["Average_DL_bitrate", "Average_UL_bitrate"], inplace=False):
+        groups = self.__df.groupby("session")
+        for name, group in groups:
+            x, y = self.create_sequences(dataframe=group, history_length=history_length,
+             horizon_length=horizon_length, exclude_columns=exclude_columns,
+              include_columns=include_columns, y_columns=y_columns, inplace=False)
+            self.__x_sequences = self.__x_sequences + x
+            self.__y_sequences = self.__y_sequences + y
+        self.__x_sequences = np.array(self.__x_sequences)
+        self.__y_sequences = np.array(self.__y_sequences)
+
+    def create_sequences(self, dataframe=pd.DataFrame(), history_length=10, horizon_length=5, 
+     include_columns=[], exclude_columns=[],
+     y_columns=["Average_DL_bitrate", "Average_UL_bitrate"], inplace=False):
+
+        if dataframe.empty:
+            dataframe = self.__df
+        if exclude_columns:
+            include_columns = list(set(dataframe.columns).difference(exclude_columns))
+        data = dataframe[include_columns]
+        new_order = y_columns+list(set(include_columns).difference(y_columns))
+        data = data.reindex(columns=new_order)
+        x_sequences = []
+        y_sequences = []
+        for i in range(history_length, len(data)-horizon_length+1):
+            x_sequences.append(data[i-history_length:i])
+            # Not so sure about this ask for assistance.
+            y_sequences.append(data[y_columns][i:i+horizon_length])
+        if inplace:
+            self.__x_sequences = np.array(x_sequences)
+            self.__y_sequences = np.array(y_sequences)
+            return None, None
+        return x_sequences, y_sequences
+
     def get_df(self):
         return self.__df
 
+    def get_scaler(self):
+        return self.__scaler
+
 
 if __name__ == "__main__":
-    raw_data = pd.read_csv("C:/Users/knola/Desktop/Final Year Project/Datasets/Raw/all_4G_data.csv", index_col=None)
 
-    raw_data = raw_data #[raw_data["movement_type"] == "static"]
+    # Transform from raw data
+    # raw_data = pd.read_csv("Datasets/Raw/all_4G_data.csv", index_col=None)
+    # imputed_data = WirelessDataPreProcessor(raw_data).get_df()
+    # imputed_data.to_csv("Datasets/Transformed/4G_data_ASN.csv", index=False, encoding="utf-8")
 
-    imputed_data = WirelessDataPreProcessor(raw_data).get_df()
-
-    imputed_data.to_csv("transformed_4G_data.csv", index=False, encoding="utf-8")
+    # import transformed data and create sequences.
+    data = pd.read_csv("Datasets/Transformed/4G_data_ASN.csv", index_col=None, encoding="utf-8")
+    data_processor = WirelessDataPreProcessor(data, manual_mode=True)
+    test_df = data_processor.get_df()
+    test_df = test_df[test_df["session"]==0]
+    x, y = data_processor.create_sequences(dataframe=test_df,
+    exclude_columns=["Timestamp", "CellID", "DL_bitrate", "UL_bitrate"],
+     y_columns=["Average_DL_bitrate", "Average_UL_bitrate"])
+    print(y)
