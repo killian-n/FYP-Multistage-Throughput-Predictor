651dea520c4bd7cfd8af116c282f46dd1fea7f88
Killian <knolan9119@gmail.com>
2023-01-22 17:18:42 +0000

    Added getters for input and output shape of model

--- src/models/model_framework.py
+++ src/models/model_framework.py
@@ -14,6 +14,9 @@ class ModelFramework(ABC):
         self._test_x = []
         self._test_y = []
         self._results = None
+        self._input_shape = ()
+        self._output_shape = ()
+        self._train_time = 0
 
     @abstractclassmethod
     def build_model(self):
@@ -38,5 +41,27 @@ class ModelFramework(ABC):
         self._results = self._model.evaluate(self._test_x, self._test_y, batch_size=100)
         print(self._model_name, "\n===============\n",self._model.metrics_names, "\n===============\n" ,self._results, "\n===============\n")
 
+    def set_model(self, model):
+        self._model = model
+        self.set_input_shape()
+        self.set_output_shape()
+
     def get_model(self):
         return self._model
+
+    def set_input_shape(self):
+        for layer in self._model.layers:
+            self._input_shape = layer.output_shape
+            break
+    
+    def set_output_shape(self):
+        for layer in self._model.layers:
+            shape = layer.output_shape
+        self._output_shape = shape
+
+    def get_input_shape(self):
+        return self._input_shape
+
+    def get_output_shape(self):
+        return self._output_shape
+
