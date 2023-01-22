f4fd8f9b7d334a3b566fb4bfa7872edc61519a8b
Killian <knolan9119@gmail.com>
2023-01-22 17:16:50 +0000

    Simple train time callback for models.

---
+++ src/helper_functions/timer.py
@@ -0,0 +1,10 @@
+from keras.callbacks import Callback
+from timeit import default_timer as timer
+
+class TimingCallback(Callback):
+    def __init__(self, logs={}):
+        self.logs=[]
+    def on_epoch_begin(self, epoch, logs={}):
+        self.starttime = timer()
+    def on_epoch_end(self, epoch, logs={}):
+        self.logs.append(timer()-self.starttime)
