121980570edbf05b19fd2c18791430501855a51a
Killian <knolan9119@gmail.com>
2023-01-28 19:57:51 +0000

    Reran models which broke due to preprocessor bug.

--- src/main.py
+++ src/main.py
@@ -14,42 +14,54 @@ from models.multi_selection_multistage_predictor import MultiSelectionMultistage
 
 if __name__ == "__main__":
     raw_data = pd.read_csv("Datasets/Raw/all_4G_data.csv", encoding="utf-8")
+    # GET BASELINE PREFORMANCE LATER
+    # preprocessor_baseline = DataPreProcessor(raw_data)
 
-    preprocessor_univariate = DataPreProcessor(raw_data) 
-    SSMSP = SingleSelectionMultistagePredictor(raw_data, preprocessor=preprocessor_univariate)
-    MSMSP = MultiSelectionMultistagePredictor(raw_data, preprocessor=preprocessor_univariate)
+    # preprocessor_univariate = DataPreProcessor(raw_data) 
+    # SSMSP = SingleSelectionMultistagePredictor(preprocessor=preprocessor_univariate)
+    # MSMSP = MultiSelectionMultistagePredictor(preprocessor=preprocessor_univariate)
 
-    preprocessor_cqi = DataPreProcessor(raw_data, include_features=["CQI"])
-    SSMSP_CQI = SingleSelectionMultistagePredictor(raw_data, preprocessor=preprocessor_cqi, model_name="SSMSP_CQI")
-    MSMSP_CQI = MultiSelectionMultistagePredictor(raw_data, preprocessor=preprocessor_cqi, model_name="MSMSP_CQI")
+    # preprocessor_RSRQ = DataPreProcessor(raw_data, include_features=["RSRQ"])
+    # SSMSP_RSRQ = SingleSelectionMultistagePredictor(preprocessor=preprocessor_RSRQ, model_name="SSMSP_RSRQ")
+    # MSMSP_RSRQ = MultiSelectionMultistagePredictor(preprocessor=preprocessor_RSRQ, model_name="MSMSP_RSRQ")
 
-    preprocessor_snr = DataPreProcessor(raw_data, include_features=["SNR"])
-    SSMSP_SNR = SingleSelectionMultistagePredictor(raw_data, preprocessor=preprocessor_snr, model_name="SSMSP_SNR")
-    MSMSP_SNR = MultiSelectionMultistagePredictor(raw_data, preprocessor=preprocessor_snr, model_name="MSMSP_SNR")
+    # preprocessor_RSRP = DataPreProcessor(raw_data, include_features=["RSRP"])
+    # SSMSP_RSRP = SingleSelectionMultistagePredictor(preprocessor=preprocessor_RSRP, model_name="SSMSP_RSRP")
+    # MSMSP_RSRP = MultiSelectionMultistagePredictor(preprocessor=preprocessor_RSRP, model_name="MSMSP_RSRP")
+
+    preprocessor_SNR = DataPreProcessor(raw_data, include_features=["SNR"])
+    SSMSP_SNR = SingleSelectionMultistagePredictor(preprocessor=preprocessor_SNR, model_name="SSMSP_SNR")
+    MSMSP_SNR = MultiSelectionMultistagePredictor(preprocessor=preprocessor_SNR, model_name="MSMSP_SNR")
+
+    preprocessor_CQI = DataPreProcessor(raw_data, include_features=["CQI"])
+    SSMSP_CQI = SingleSelectionMultistagePredictor(preprocessor=preprocessor_CQI, model_name="SSMSP_CQI")
+    MSMSP_CQI = MultiSelectionMultistagePredictor(preprocessor=preprocessor_CQI, model_name="MSMSP_CQI")
     
-    # increasing horizon length to 10
-    preprocessor_univariate_h10h10 = DataPreProcessor(raw_data, history=10, horizon=10)
-    SSMSP_h10h10 = SingleSelectionMultistagePredictor(raw_data, preprocessor=preprocessor_univariate_h10h10, model_name="SSMSP_h10h10")
-    MSMSP_h10h10 = MultiSelectionMultistagePredictor(raw_data, preprocessor=preprocessor_univariate_h10h10, model_name="MSMSP_h10h10")
+    # # Increasing history length to 15
+    # preprocessor_univariate_h15h5 = DataPreProcessor(raw_data, history=15, horizon=5)
+    # SSMSP_h15h5 = SingleSelectionMultistagePredictor(preprocessor=preprocessor_univariate_h15h5, model_name="SSMSP_h15h5")
+    # MSMSP_h15h5 = MultiSelectionMultistagePredictor(preprocessor=preprocessor_univariate_h15h5, model_name="MSMSP_h15h5")
 
-    # increasing horizon length to 15
-    preprocessor_univariate_h10h15 = DataPreProcessor(raw_data, history=10, horizon=15)
-    SSMSP_h10h15 = SingleSelectionMultistagePredictor(raw_data, preprocessor=preprocessor_univariate_h10h10, model_name="SSMSP_h10h15")
-    MSMSP_h10h15 = MultiSelectionMultistagePredictor(raw_data, preprocessor=preprocessor_univariate_h10h10, model_name="MSMSP_h10h15")
+    # # Increasing history length to 20
+    # preprocessor_univariate_h20h5 = DataPreProcessor(raw_data, history=20, horizon=5)
+    # SSMSP_h20h5 = SingleSelectionMultistagePredictor(preprocessor=preprocessor_univariate_h20h5, model_name="SSMSP_h20h5")
+    # MSMSP_h20h5 = MultiSelectionMultistagePredictor(preprocessor=preprocessor_univariate_h20h5, model_name="MSMSP_h20h5")
 
-    models_to_test = [SSMSP, MSMSP, SSMSP_CQI, MSMSP_CQI, SSMSP_SNR, MSMSP_SNR, SSMSP_h10h10, MSMSP_h10h10, SSMSP_h10h15, MSMSP_h10h15]
+    # # Increasing horizon length to 10
+    # preprocessor_univariate_h10h10 = DataPreProcessor(raw_data, history=10, horizon=10)
+    # SSMSP_h10h15 = SingleSelectionMultistagePredictor(preprocessor=preprocessor_univariate_h10h10, model_name="SSMSP_h10h10")
+    # MSMSP_h10h15 = MultiSelectionMultistagePredictor(preprocessor=preprocessor_univariate_h10h10, model_name="MSMSP_h10h10")
 
-    # models_to_test = [SSMSP_CQI]
+    # models_to_test = [SSMSP, MSMSP, SSMSP_RSRQ, MSMSP_RSRQ, SSMSP_RSRP, MSMSP_RSRP, SSMSP_SNR, MSMSP_SNR, SSMSP_CQI, MSMSP_CQI, SSMSP_h15h5, MSMSP_h15h5, SSMSP_h20h5, MSMSP_h20h5, SSMSP_h10h15, MSMSP_h10h15]
 
-    with open("Datasets/results.csv", "w") as f:
+    models_to_test = [SSMSP_SNR, MSMSP_SNR, SSMSP_CQI, MSMSP_CQI]
+
+    with open("Datasets/results.csv", "a", newline="") as f:
         writer = csv.writer(f)
-        writer.writerow(["model_name", "trainable_params", "non_trainable_params", "train_time", "predict_time", "mse", "mae", "average_bias", "test_size"])
+        # writer.writerow(["model_name", "trainable_params", "non_trainable_params", "train_time", "predict_time", "mse", "mae", "average_bias", "test_size"])
         for model in models_to_test:
             model.pre_process()
             model.build_and_train()
             model.test()
             results = model.get_performance_metrics()
             writer.writerow(results)          
-
-
-
