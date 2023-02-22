e2ea1a622ec1ed7bcd9eb3ee75b0164b698e120a
Killian <knolan9119@gmail.com>
2023-02-22 00:03:21 +0000

    Updated main to carry out testing.

--- src/main.py
+++ src/main.py
@@ -11,55 +11,50 @@ sys.path.append(module_path)
 from data_transformation.preprocessor import DataPreProcessor
 from models.single_selection_multistage_predictor import SingleSelectionMultistagePredictor
 from models.multi_selection_multistage_predictor import MultiSelectionMultistagePredictor
+from models.simple_LSTM import SimpleLSTM
+from models.complex_LSTM import ComplexLSTM
 
 if __name__ == "__main__":
     raw_data = pd.read_csv("Datasets/Raw/all_4G_data.csv", encoding="utf-8")
-    # GET BASELINE PREFORMANCE LATER
-    # preprocessor_baseline = DataPreProcessor(raw_data)
 
+    # Univariate
     preprocessor_univariate = DataPreProcessor(raw_data)
-    SSMSP = SingleSelectionMultistagePredictor(preprocessor=preprocessor_univariate)
-    MSMSP = MultiSelectionMultistagePredictor(preprocessor=preprocessor_univariate)
+    multi_one_univariate = SingleSelectionMultistagePredictor(preprocessor=preprocessor_univariate)
+    multi_all_univariate = MultiSelectionMultistagePredictor(preprocessor=preprocessor_univariate)
+    base_univariate = ComplexLSTM(preprocessor=preprocessor_univariate, model_name="base_univariate")
 
-    preprocessor_RSRQ = DataPreProcessor(raw_data, include_features=["RSRQ"])
-    SSMSP_RSRQ = SingleSelectionMultistagePredictor(preprocessor=preprocessor_RSRQ, model_name="SSMSP_RSRQ")
-    MSMSP_RSRQ = MultiSelectionMultistagePredictor(preprocessor=preprocessor_RSRQ, model_name="MSMSP_RSRQ")
+    # 1
+    # Multivariate with RSRQ
+    preprocessor_multi1 = DataPreProcessor(raw_data, include_features=["RSRQ"], scaler_file_name="multi_1_scaler.sav")
+    multi_one_multi1 = SingleSelectionMultistagePredictor(preprocessor=preprocessor_multi1, model_name="multi_one_1")
+    multi_all_multi1 = MultiSelectionMultistagePredictor(preprocessor=preprocessor_multi1, model_name="multi_all_1")
+    base_multi1 = ComplexLSTM(preprocessor=preprocessor_multi1, model_name="base_1")
 
-    preprocessor_RSRP = DataPreProcessor(raw_data, include_features=["RSRP"])
-    SSMSP_RSRP = SingleSelectionMultistagePredictor(preprocessor=preprocessor_RSRP, model_name="SSMSP_RSRP")
-    MSMSP_RSRP = MultiSelectionMultistagePredictor(preprocessor=preprocessor_RSRP, model_name="MSMSP_RSRP")
+    # 2
+    # Multivariate with RSRQ, Longitude, Latitude, State, State
+    preprocessor_multi2 = DataPreProcessor(raw_data, include_features=["RSRQ", "Longitude", "Latitude", "State"], scaler_file_name="multi_2_scaler.sav")
+    multi_one_multi2 = SingleSelectionMultistagePredictor(preprocessor=preprocessor_multi2, model_name="multi_one_2")
+    multi_all_multi2 = MultiSelectionMultistagePredictor(preprocessor=preprocessor_multi2, model_name="multi_all_2")
+    base_multi2 = ComplexLSTM(preprocessor=preprocessor_multi2, model_name="base_2")
 
-    preprocessor_SNR = DataPreProcessor(raw_data, include_features=["SNR"])
-    SSMSP_SNR = SingleSelectionMultistagePredictor(preprocessor=preprocessor_SNR, model_name="SSMSP_SNR")
-    MSMSP_SNR = MultiSelectionMultistagePredictor(preprocessor=preprocessor_SNR, model_name="MSMSP_SNR")
+    # 3
+    # Multivariate with RSRQ, Longitude, Latitude, State, State, NRxRSRQ
+    preprocessor_multi3 = DataPreProcessor(raw_data, include_features=["RSRQ", "Longitude", "Latitude", "State", "NRxRSRQ"], scaler_file_name="multi_3_scaler.sav")
+    multi_one_multi3 = SingleSelectionMultistagePredictor(preprocessor=preprocessor_multi3, model_name="multi_one_3")
+    multi_all_multi3 = MultiSelectionMultistagePredictor(preprocessor=preprocessor_multi3, model_name="multi_all_3")
+    base_multi3 = ComplexLSTM(preprocessor=preprocessor_multi3, model_name="base_3")
 
-    preprocessor_CQI = DataPreProcessor(raw_data, include_features=["CQI"])
-    SSMSP_CQI = SingleSelectionMultistagePredictor(preprocessor=preprocessor_CQI, model_name="SSMSP_CQI")
-    MSMSP_CQI = MultiSelectionMultistagePredictor(preprocessor=preprocessor_CQI, model_name="MSMSP_CQI")
-    
-    # Increasing history length to 15
-    preprocessor_univariate_h15h5 = DataPreProcessor(raw_data, history=15, horizon=5)
-    SSMSP_h15h5 = SingleSelectionMultistagePredictor(preprocessor=preprocessor_univariate_h15h5, model_name="SSMSP_h15h5")
-    MSMSP_h15h5 = MultiSelectionMultistagePredictor(preprocessor=preprocessor_univariate_h15h5, model_name="MSMSP_h15h5")
+    models_to_test = [base_univariate, multi_one_univariate, base_multi1, multi_one_multi1, base_multi2, multi_one_multi2, base_multi3, multi_one_multi3, multi_all_univariate, multi_all_multi1, multi_all_multi2, multi_all_multi3]
 
-    # Increasing history length to 20
-    preprocessor_univariate_h20h5 = DataPreProcessor(raw_data, history=20, horizon=5)
-    SSMSP_h20h5 = SingleSelectionMultistagePredictor(preprocessor=preprocessor_univariate_h20h5, model_name="SSMSP_h20h5")
-    MSMSP_h20h5 = MultiSelectionMultistagePredictor(preprocessor=preprocessor_univariate_h20h5, model_name="MSMSP_h20h5")
-
-    # Increasing horizon length to 10
-    preprocessor_univariate_h10h10 = DataPreProcessor(raw_data, history=10, horizon=10)
-    SSMSP_h10h15 = SingleSelectionMultistagePredictor(preprocessor=preprocessor_univariate_h10h10, model_name="SSMSP_h10h10")
-    MSMSP_h10h15 = MultiSelectionMultistagePredictor(preprocessor=preprocessor_univariate_h10h10, model_name="MSMSP_h10h10")
-
-    models_to_test = [SSMSP, MSMSP, SSMSP_RSRQ, MSMSP_RSRQ, SSMSP_RSRP, MSMSP_RSRP, SSMSP_SNR, MSMSP_SNR, SSMSP_CQI, MSMSP_CQI, SSMSP_h15h5, MSMSP_h15h5, SSMSP_h20h5, MSMSP_h20h5, SSMSP_h10h15, MSMSP_h10h15]
-
-    with open("Datasets/new_results.csv", "a", newline="") as f:
+    # models_to_test = [base_multi1]
+    with open(config["metrics"]["RESULTS_PATH"], "w", newline="") as f:
         writer = csv.writer(f)
-        writer.writerow(["model_name", "trainable_params", "non_trainable_params", "train_time", "predict_time", "mse", "mae", "average_bias", "test_size"])
-        for model in models_to_test:
-            model.pre_process()
+        writer.writerow(["model_name", "trainable_params", "non_trainable_params", "train_time", "predict_time", "mse", "mae", "average_bias", "mape", "model_size", "accuracy"])
+    for model in models_to_test:
+        model.pre_process()
+        try:
             model.build_and_train()
-            model.test()
-            results = model.get_performance_metrics()
-            writer.writerow(results)
+        except:
+            model.build_model()
+            model.train()
+        model.test()
