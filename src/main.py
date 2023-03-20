2a40f710a4213e652974f696cc948084cb750903
Killian <knolan9119@gmail.com>
2023-03-20 10:48:53 +0000

    Fyp paper directory

--- src/main.py
+++ src/main.py
@@ -19,48 +19,15 @@ if __name__ == "__main__":
 
     # Univariate
     preprocessor_univariate = DataPreProcessor(raw_data)
-    multi_one_univariate = SingleSelectionMultistagePredictor(preprocessor=preprocessor_univariate, model_name="unop_univariate_multiOne")
-    univariate_baseline = BaselineLSTM(preprocessor=preprocessor_univariate, model_name="unop_univariate_baseline")
+    multi_one_univariate = SingleSelectionMultistagePredictor(preprocessor=preprocessor_univariate, model_name="univariate_multiOne")
+    univariate_baseline = BaselineLSTM(preprocessor=preprocessor_univariate, model_name="univariate_baseline")
+    models_to_test = [multi_one_univariate, univariate_baseline]
 
-    # # 1
-    # # Multivariate with RSRQ
-    # preprocessor_multi1 = DataPreProcessor(raw_data, include_features=["RSRQ"], scaler_file_name="multi_1_scaler.sav")
-    # multi_one_multi1 = SingleSelectionMultistagePredictor(preprocessor=preprocessor_multi1, model_name="multi_one_1")
-    # base_multi1 = ComplexLSTM(preprocessor=preprocessor_multi1, model_name="base_1")
-
-    # 2
-    # Multivariate with RSRQ, Longitude, Latitude, State, State
-    # preprocessor_multi2 = DataPreProcessor(raw_data, include_features=["RSRQ", "Longitude", "Latitude", "State"], scaler_file_name="multi_2_scaler.sav")
-    # multi_one_multi2 = SingleSelectionMultistagePredictor(preprocessor=preprocessor_multi2, model_name="multi_one_2")
-    # base_multi2 = ComplexLSTM(preprocessor=preprocessor_multi2, model_name="base_2")
-
-    # 3
-    # Multivariate with RSRQ, Longitude, Latitude, State, NRxRSRQ
-    # preprocessor_multi3 = DataPreProcessor(raw_data, include_features=["RSRQ", "Longitude", "Latitude", "State", "NRxRSRQ"], scaler_file_name="multi_3_scaler.sav")
-    # multi_one_multi3 = SingleSelectionMultistagePredictor(preprocessor=preprocessor_multi3, model_name="multi_one_3")
-    # base_multi3 = ComplexLSTM(preprocessor=preprocessor_multi3, model_name="base_3")
-
-    # multi_all_univariate = MultiSelectionMultistagePredictor(preprocessor=preprocessor_univariate,model_name="multi_all_univariate", pretrained_sequence_models="multi_one_univariate")
-    # multi_all_multi1 = MultiSelectionMultistagePredictor(preprocessor=preprocessor_multi1, model_name="multi_all_1", pretrained_sequence_models="multi_one_1")
-    # multi_all_multi2 = MultiSelectionMultistagePredictor(preprocessor=preprocessor_multi2, model_name="multi_all_2", pretrained_sequence_models="multi_one_2")
-    # multi_all_multi3 = MultiSelectionMultistagePredictor(preprocessor=preprocessor_multi3, model_name="multi_all_3", pretrained_sequence_models="multi_one_3")
-    
-    # models_to_test = [base_univariate, multi_one_univariate, base_multi1, multi_one_multi1, base_multi2, multi_one_multi2, base_multi3,
-    #  multi_one_multi3, multi_all_univariate, multi_all_multi1, multi_all_multi2, multi_all_multi3]
-
-    # 4 
-    # preprocessor_multi4 = DataPreProcessor(raw_data, include_features=["RSRQ", "NRxRSRQ", "SNR", "UL_bitrate", "NetworkMode", "State"], scaler_file_name="multi_4_scaler.sav")
-    # multi_one_multi4 = SingleSelectionMultistagePredictor(preprocessor=preprocessor_multi4, model_name="multi_one_4")
-    # base_multi4 = ComplexLSTM(preprocessor=preprocessor_multi4, model_name="base_4")
-    # multi_all_multi4 = MultiSelectionMultistagePredictor(preprocessor=preprocessor_multi4, model_name="multi_all_4", pretrained_sequence_models="multi_one_4")
-
-
-    # 5
-    all_network_preprocessor = DataPreProcessor(raw_data, scaler_file_name="all_network_features.sav", include_features=["NRxRSRQ", "RSRQ","RSRP" ,"SNR", "CQI", "RSSI", "NRxRSRP"])
-    # all_network_baseline = BaselineLSTM(preprocessor=all_network_preprocessor, model_name="unop_all_network_baseline")
-    all_network_multiOne = SingleSelectionMultistagePredictor(preprocessor=all_network_preprocessor, model_name="all_network_multiOne")
-
-    models_to_test = [all_network_multiOne]
+    # All network features
+    # all_network_preprocessor = DataPreProcessor(raw_data, scaler_file_name="all_network_features.sav", include_features=["NRxRSRQ", "RSRQ","RSRP" ,"SNR", "CQI", "RSSI", "NRxRSRP"])
+    # all_network_baseline = optimizedBaseline(preprocessor=all_network_preprocessor, model_name="all_network_baseline")
+    # all_network_multiOne = SingleSelectionMultistagePredictor(preprocessor=all_network_preprocessor, model_name="all_network_multiOne")
+    # models_to_test = [all_network_baseline]
 
     # with open(config["metrics"]["RESULTS_PATH"], "a", newline="") as f:
     #     writer = csv.writer(f)
@@ -74,14 +41,14 @@ if __name__ == "__main__":
             model.train()
         model.test()
 
-    all_network_multiAll = MultiSelectionMultistagePredictor(preprocessor=all_network_preprocessor,
-                                                            pretrained_sequence_models="all_network_multiOne",
-                                                                model_name="all_network_multiAll")
-    all_network_multiAll.pre_process()
-    all_network_multiAll.build_and_train()
-    all_network_multiAll.test()
-    # univariate_multiAll = MultiSelectionMultistagePredictor(preprocessor=preprocessor_univariate,
-    #                                                          model_name="unop_univariate_multiAll", pretrained_sequence_models="unop_univariate_multiOne")
-    # univariate_multiAll.pre_process()
-    # univariate_multiAll.build_and_train()
-    # univariate_multiAll.test()
+    # all_network_multiAll = MultiSelectionMultistagePredictor(preprocessor=all_network_preprocessor,
+    #                                                         pretrained_sequence_models="all_network_multiOne",
+    #                                                             model_name="all_network_multiAll")
+    # all_network_multiAll.pre_process()
+    # all_network_multiAll.build_and_train()
+    # all_network_multiAll.test()
+    univariate_multiAll = MultiSelectionMultistagePredictor(preprocessor=preprocessor_univariate,
+                                                             model_name="univariate_multiAll", pretrained_sequence_models="univariate_multiOne")
+    univariate_multiAll.pre_process()
+    univariate_multiAll.build_and_train()
+    univariate_multiAll.test()
