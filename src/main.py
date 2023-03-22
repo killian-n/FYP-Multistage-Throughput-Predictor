e511a616309b25a7ce4e70605c4b2a65dbce7acb
Killian <knolan9119@gmail.com>
2023-03-22 10:03:38 +0000

    New optimized models.

--- src/main.py
+++ src/main.py
@@ -18,16 +18,16 @@ if __name__ == "__main__":
     raw_data = pd.read_csv("Datasets/Raw/all_4G_data.csv", encoding="utf-8")
 
     # Univariate
-    preprocessor_univariate = DataPreProcessor(raw_data)
-    multi_one_univariate = SingleSelectionMultistagePredictor(preprocessor=preprocessor_univariate, model_name="univariate_multiOne")
-    univariate_baseline = BaselineLSTM(preprocessor=preprocessor_univariate, model_name="univariate_baseline")
-    models_to_test = [multi_one_univariate, univariate_baseline]
+    # preprocessor_univariate = DataPreProcessor(raw_data)
+    # multi_one_univariate = SingleSelectionMultistagePredictor(preprocessor=preprocessor_univariate, model_name="univariate_multiOne")
+    # univariate_baseline = BaselineLSTM(preprocessor=preprocessor_univariate, model_name="univariate_baseline")
+    # models_to_test = [multi_one_univariate, univariate_baseline]
 
     # All network features
-    # all_network_preprocessor = DataPreProcessor(raw_data, scaler_file_name="all_network_features.sav", include_features=["NRxRSRQ", "RSRQ","RSRP" ,"SNR", "CQI", "RSSI", "NRxRSRP"])
-    # all_network_baseline = optimizedBaseline(preprocessor=all_network_preprocessor, model_name="all_network_baseline")
-    # all_network_multiOne = SingleSelectionMultistagePredictor(preprocessor=all_network_preprocessor, model_name="all_network_multiOne")
-    # models_to_test = [all_network_baseline]
+    all_network_preprocessor = DataPreProcessor(raw_data, scaler_file_name="all_network_features.sav", include_features=["NRxRSRQ", "RSRQ","RSRP" ,"SNR", "CQI", "RSSI", "NRxRSRP"])
+    all_network_baseline = optimizedBaseline(preprocessor=all_network_preprocessor, model_name="st_new_all_network_baseline")
+    all_network_multiOne = SingleSelectionMultistagePredictor(preprocessor=all_network_preprocessor, model_name="st_new_all_network_multiOne")
+    models_to_test = [all_network_baseline, all_network_multiOne]
 
     # with open(config["metrics"]["RESULTS_PATH"], "a", newline="") as f:
     #     writer = csv.writer(f)
@@ -41,14 +41,15 @@ if __name__ == "__main__":
             model.train()
         model.test()
 
-    # all_network_multiAll = MultiSelectionMultistagePredictor(preprocessor=all_network_preprocessor,
-    #                                                         pretrained_sequence_models="all_network_multiOne",
-    #                                                             model_name="all_network_multiAll")
-    # all_network_multiAll.pre_process()
-    # all_network_multiAll.build_and_train()
-    # all_network_multiAll.test()
-    univariate_multiAll = MultiSelectionMultistagePredictor(preprocessor=preprocessor_univariate,
-                                                             model_name="univariate_multiAll", pretrained_sequence_models="univariate_multiOne")
-    univariate_multiAll.pre_process()
-    univariate_multiAll.build_and_train()
-    univariate_multiAll.test()
+    all_network_multiAll = MultiSelectionMultistagePredictor(preprocessor=all_network_preprocessor,
+                                                            pretrained_sequence_models="st_new_all_network_multiOne",
+                                                                model_name="st_new_all_network_multiAll")
+    all_network_multiAll.pre_process()
+    all_network_multiAll.build_and_train()
+    all_network_multiAll.test()
+
+    # univariate_multiAll = MultiSelectionMultistagePredictor(preprocessor=preprocessor_univariate,
+    #                                                          model_name="univariate_multiAll", pretrained_sequence_models="univariate_multiOne")
+    # univariate_multiAll.pre_process()
+    # univariate_multiAll.build_and_train()
+    # univariate_multiAll.test()
