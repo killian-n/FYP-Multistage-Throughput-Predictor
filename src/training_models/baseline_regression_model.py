f839613d95ae34ac8692cf05c34e7a5482309f31
Killian <knolan9119@gmail.com>
2023-06-18 19:29:49 +0100

    changed epoch from 60 to 70.

--- src/training_models/baseline_regression_model.py
+++ src/training_models/baseline_regression_model.py
@@ -54,7 +54,7 @@ class BaselineLSTM(ModelFramework):
         self.set_input_shape()
         self.set_output_shape()
 
-    def train(self, epochs=60, batch_size=32, validation_split=0.2):
+    def train(self, epochs=70, batch_size=32, validation_split=0.2):
         project_path = config["global"]["PROJECT_PATH"]
         if project_path[-1] not in ["\\", "/"]:
             project_path += "/"
