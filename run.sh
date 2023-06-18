2d76e0a099bb428183e175879aa4c0d108b10147
Killian <knolan9119@gmail.com>
2023-06-18 18:22:36 +0100

    removed .exe from python.

--- run.sh
+++ run.sh
@@ -19,28 +19,28 @@ cd src/data_transformation
 cd ../training_models
 run_prefix=standardised
 data_prefix=multivariate
-python.exe train_model.py --prefix $run_prefix --model "baseline" --data_prefix $data_prefix
-python.exe train_model.py --prefix $run_prefix --model "high" --data_prefix $data_prefix
-python.exe train_model.py --prefix $run_prefix --model "medium" --data_prefix $data_prefix
-python.exe train_model.py --prefix $run_prefix --model "low" --data_prefix $data_prefix
-python.exe train_model.py --prefix $run_prefix --model "classifier" --data_prefix $data_prefix
+python train_model.py --prefix $run_prefix --model "baseline" --data_prefix $data_prefix
+python train_model.py --prefix $run_prefix --model "high" --data_prefix $data_prefix
+python train_model.py --prefix $run_prefix --model "medium" --data_prefix $data_prefix
+python train_model.py --prefix $run_prefix --model "low" --data_prefix $data_prefix
+python train_model.py --prefix $run_prefix --model "classifier" --data_prefix $data_prefix
 
 cd ../trained_models
-python.exe test_model.py --model_prefix $run_prefix --model "baseline" --data_prefix $data_prefix
-python.exe test_model.py --model_prefix $run_prefix --model "multiOne" --data_prefix $data_prefix
-python.exe test_model.py --model_prefix $run_prefix --model "multiAll" --data_prefix $data_prefix
+python test_model.py --model_prefix $run_prefix --model "baseline" --data_prefix $data_prefix
+python test_model.py --model_prefix $run_prefix --model "multiOne" --data_prefix $data_prefix
+python test_model.py --model_prefix $run_prefix --model "multiAll" --data_prefix $data_prefix
 
 cd ../training_models
 run_prefix=standardised
 data_prefix=univariate
-python.exe train_model.py --prefix $run_prefix --model "baseline" --data_prefix $data_prefix
-python.exe train_model.py --prefix $run_prefix --model "high" --data_prefix $data_prefix
-python.exe train_model.py --prefix $run_prefix --model "medium" --data_prefix $data_prefix
-python.exe train_model.py --prefix $run_prefix --model "low" --data_prefix $data_prefix
-python.exe train_model.py --prefix $run_prefix --model "classifier" --data_prefix $data_prefix
+python train_model.py --prefix $run_prefix --model "baseline" --data_prefix $data_prefix
+python train_model.py --prefix $run_prefix --model "high" --data_prefix $data_prefix
+python train_model.py --prefix $run_prefix --model "medium" --data_prefix $data_prefix
+python train_model.py --prefix $run_prefix --model "low" --data_prefix $data_prefix
+python train_model.py --prefix $run_prefix --model "classifier" --data_prefix $data_prefix
 
 cd ../trained_models
-python.exe test_model.py --model_prefix $run_prefix --model "baseline" --data_prefix $data_prefix
-python.exe test_model.py --model_prefix $run_prefix --model "multiOne" --data_prefix $data_prefix
-python.exe test_model.py --model_prefix $run_prefix --model "multiAll" --data_prefix $data_prefix
+python test_model.py --model_prefix $run_prefix --model "baseline" --data_prefix $data_prefix
+python test_model.py --model_prefix $run_prefix --model "multiOne" --data_prefix $data_prefix
+python test_model.py --model_prefix $run_prefix --model "multiAll" --data_prefix $data_prefix
 
